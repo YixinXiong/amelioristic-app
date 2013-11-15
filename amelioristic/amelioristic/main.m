c39babd1a342e5367beaffa84bd438695e25b0f4
YihanLu <jacky_yihan_lu@hotmail.com>
2013-11-14 22:13:49 -0500

    Initialize project

---
+++ amelioristic/amelioristic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  amelioristic
+//
+//  Created by Jacky on 11/14/13.
+//  Copyright (c) 2013 Jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
